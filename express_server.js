80b7156e4d1034a737f4e1fcb2ce836ea8cffc1a
Tyler Krys <tylerkrys@gmail.com>
2017-07-04 20:39:56 +0000

    Adds route to /urls/new in express_server.js and a POST method

--- express_server.js
+++ express_server.js
@@ -32,6 +32,14 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

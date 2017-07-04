25a936b242ee55d7e266c8c653aa98388ccd02b5
Tyler Krys <tylerkrys@gmail.com>
2017-07-04 20:38:31 +0000

    Requires body-parser in express_server.js and creates a variable to hold its contents

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"

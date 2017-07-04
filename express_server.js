3f56d46dfcc0e7b91a58508708f6949077445ca4
Tyler Krys <tylerkrys@gmail.com>
2017-07-04 20:18:14 +0000

    Adds EJS as a view engine in express_server.js

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs")
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"

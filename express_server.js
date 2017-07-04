6472a49bd327151810dcbed6da5e74a1e54daae5
Tyler Krys <tylerkrys@gmail.com>
2017-07-04 20:24:21 +0000

    Adds views/urls_show.ejs to show detail about an input URL, and a route to it in express_server.js

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

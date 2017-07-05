e5debc271c60f89cbbb12357eb663687e9fa5ab5
Tyler Krys <tylerkrys@gmail.com>
2017-07-05 18:45:54 +0000

    Updates express_server.js variables to ES6 format

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Instead of an actual database, use a dummy database object for now
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };

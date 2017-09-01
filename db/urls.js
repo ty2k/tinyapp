0a059fd07c32f9fee344c9b77bae582f585ace4d
Tyler Krys <tylerkrys@gmail.com>
2017-09-01 01:50:52 +0000

    Refactors user and URL database objects into separate files

---
+++ db/urls.js
@@ -0,0 +1,13 @@
+// Instead of an actual URL database, use a dummy database object for now
+const urlDatabase = {
+  "b2xVn2": {
+    id: "b2xVn2",
+    userID: "userRandomID",
+    url: "http://www.lighthouselabs.ca"
+  },
+  "9sm5xK": {
+    id: "9sm5xK",
+    userID: "user2RandomID",
+    url: "http://www.google.com"
+  }
+};

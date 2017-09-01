435527204d977bc8cc12d95dd93dfafac2c438f4
Tyler Krys <tylerkrys@gmail.com>
2017-09-01 01:51:28 +0000

    Refactors outbound shortened link route into routes/u.js

---
+++ routes/u.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const users = require("../db/users");
+const urlDatabase = require("../db/urls");
+
+// GET route for redirection out
+router.get("/:shortURL", (req, res) => {
+  // If the shortURL is invalid, display an error page
+  if (!urlDatabase[req.params.shortURL]) {
+    let templateVars = {
+      user: users[req.session.user_id]
+    };
+    return res.render("urls_invalid", templateVars);
+  // Otherwise direct to the long URL
+  } else {
+    let longURL = urlDatabase[req.params.shortURL].url;
+    res.redirect(longURL);
+  }
+});
+
+module.exports = router;
